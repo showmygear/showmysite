b9a343df0f73cb0baafddf84516b5f4d848df435
Wanyu Chang <2and3and4and1@gmail.com>
2021-12-02 19:44:02 +0800

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,25 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.showmygear.com",
-    title: "曬裝備 - 登山裝備檢查表",
+    title: `曬裝備 - 登山裝備檢查表`,
+    description:
+      `曬裝備是一個方便分享給其他人的登山裝備檢查表`,
+    url: `https://www.showmygear.com`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `曬裝備 - 登山裝備檢查表`,
+        short_name: `曬裝備`,
+        description: `The application does cool things and makes your life better.`,
+        lang: `zh-tw`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#fff`,
+      }
+    },
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
